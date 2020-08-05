bd2bc7fd4b0f9e6977b3617f31bee178257e41cf
abhi91420 <abhi91420@gmail.com>
2020-08-05 11:17:42 -0400

    Stack like task managment - Done

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ class Gui:
         self.clock.grid(row = 0,column = 0,padx = 80,pady = 40)
         self.update_clock()
         self.new_button = Button(self.root, text="New",command = self.newwindow).grid(row = 0, column =1)
-        self.r =0
+        self.r =1
+        self.label_list = []
+        self.flag = 0
+        Label(self.root, text="add tasks in ascending order", pady=10, fg='red').grid()
+
     # clock
     def update_clock(self):
         now = strftime("%H:%M:%S")
@@ -25,6 +29,7 @@ class Gui:
     def label(self, txt):
         self.l = Label(self.root, text=txt, fg="red",pady =15)
         self.l.grid(row = self.r, column =0)
+        self.label_list.append(self.l)
 
     # button creator
     def donebutton(self):
@@ -35,16 +40,17 @@ class Gui:
     def task(self,txt):
         self.r +=1
         self.label(txt)
-        self.donebutton()
-
+        if (self.flag == 0):
+            self.donebutton()
+        self.flag = 1
     # delete task
     def del_task(self):
-        self.l.destroy()
-        self.b.destroy()
+        self.label_list.pop(0).destroy()
 
     # display gui method
     def display(self):
         self.root.mainloop()
+        print(self.time_list)
 
     # new window
     def newwindow(self):
@@ -58,7 +64,7 @@ class Gui:
         self.t1.grid()
         Label(self.newwindow, text="Time").grid()
         self.t2.grid()
-        self.c_b=Button(self.newwindow,text = "CREATE",command = lambda : self.task(self.t1.get("1.0",END)))
+        self.c_b=Button(self.newwindow,text = "CREATE",command = lambda : self.task(self.t1.get("1.0",END)+"DUE @"+self.t2.get("1.0", END)))
         self.c_b.grid()
 
 if __name__ == '__main__':
